af2af0c61f5b293f42c49a73bcff0679c28544cd
Michael Vorburger <vorburger@redhat.com>
2017-03-23 19:13:16 +0100

    Daexim export faster

--- Daexim/daexim-schedule-export.sh
+++ Daexim/daexim-schedule-export.sh
@@ -2,6 +2,6 @@ set -x
 rm -v ../../../git/netvirt/vpnservice/distribution/karaf/target/assembly/daexim/*
 curl -u admin:admin --header Content-Type:application/json --data @daexim-schedule-export-POST.json http://localhost:8181/restconf/operations/data-export-import:schedule-export
 # Could use Daexim status API to see when it's done...
-sleep 20
+sleep 5
 # cat ../../../git/netvirt/vpnservice/distribution/karaf/target/assembly/daexim/odl_backup_config.json | python -mjson.tool >  config.json
 # cat ../../../git/netvirt/vpnservice/distribution/karaf/target/assembly/daexim/odl_backup_operational.json | python -mjson.tool >  op.json
